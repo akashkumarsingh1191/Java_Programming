79906fe72a2af1f03b21f4662b6bf6b29a139b47
Akash Kumar Singh <akashkumarsingh1191@gmail.com>
2023-08-16 22:17:23 +0530

    Basic2

--- Basic.java
+++ Basic.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class Basic{
     public static void main(String args[]){
         int X1,X2,Y1,Y2;
-        System.out.println("Enter the (x1,y1) coordinate:");
         Scanner sc=new Scanner(System.in);
         X1=sc.nextInt();
         Y1=sc.nextInt();
@@ -13,5 +12,22 @@ public class Basic{
         int Height=Y2-Y1;
         int Area=Length*Height;
         System.out.println("Area of Rectangle is : " +Area);
+
+        System.out.println("Ques 2:---------->>>><<<<<<<------------");
+        char ch;
+        int m1,m2,m3;
+        System.out.println("Enter the first Character of the name : ");
+        ch=sc.next().charAt(0);
+        System.out.println("Enter the first marks of the subject : ");
+        m1=sc.nextInt();
+        System.out.println("Enter the second marks of the subject : ");
+        m2=sc.nextInt();
+        System.out.println("Enter the third marks of the subject : ");
+        m3=sc.nextInt();
+        int avg=(m1+m2+m3)/3;
+        System.out.println(ch+" "+avg);
+
+
+        
     }
 }
