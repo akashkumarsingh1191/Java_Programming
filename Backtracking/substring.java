607ab32c1fa9cd1ebd674ae5e2071d07761e76fe
Akash <akashkumarsingh1191@gmail.com>
2023-11-01 06:26:48 +0530

    backtracking_prob-3

--- Backtracking/substring.java
+++ Backtracking/substring.java
@@ -15,9 +15,22 @@ public class substring{
         // No Choice
         findSubset(str, ans, i+1);
     }
+
+    public static void findPermu(String str, String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char curr=str.charAt(i);
+            String NewStr=str.substring(0, i)+str.substring(i+1);
+            findPermu(NewStr, ans+curr);
+        }
+    }
     public static void main(String args[]){
         String str="abc";
-        findSubset(str, "", 0);
+        // findSubset(str, "", 0);
+        findPermu(str, "");
 
     }
 }
