ad2ead533eb32f9e02f9ac44b737d3bd87301435
Akash <akashkumarsingh1191@gmail.com>
2023-11-20 20:20:46 +0530

    ArrayList_Intro

---
+++ ArrayList/Intro.java
@@ -0,0 +1,14 @@
+package ArrayList;
+
+
+import java.util.ArrayList;
+
+public class Intro{
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+    }
+}
