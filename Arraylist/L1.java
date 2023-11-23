9be9121f301de8f8f534cc54db35d64cc75ef34e
Akash <akashkumarsingh1191@gmail.com>
2023-11-23 12:46:50 +0530

    Basic ArrayLIst

---
+++ Arraylist/L1.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+import java.util.*;
+public class L1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        ArrayList<Boolean> list2=new ArrayList<>();
+    }
+}
