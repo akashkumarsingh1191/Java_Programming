350925f987311f1c89758fd1b9fc971374e46076
Akash <akashkumarsingh1191@gmail.com>
2023-12-03 10:38:21 +0530

    Add node at head,tail,middle in Linkedlist

--- Linked_List/LinkedList.java
+++ Linked_List/LinkedList.java
@@ -29,10 +29,64 @@ public class LinkedList {
         // step 3 - head = newNode
         head=newNode;
     }
+
+    public void addLast(int data){
+        // create a new node
+        Node newNode=new Node(data);
+        if(head == null){
+            head=tail=newNode;
+            return;
+        }
+        // tail.next <<-- new node
+        tail.next=newNode;
+        // new node <<-- tail
+        tail =newNode;
+
+    }
+
+    public void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp=head;
+        while (temp!=null) {
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void add(int idx, int data){
+        if(idx==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode=new Node(data);
+        Node temp=head;
+        int i=0;
+        while(i < idx-1){
+            temp=temp.next;
+            i++;
+        }
+        // i = idx-1; temp -> prev
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
-        ll.addFirst(1);
+        // ll.print();
         ll.addFirst(2);
+        // ll.print();
+        ll.addFirst(1);
+        // ll.print();
+        ll.addLast(3);
+        // ll.print();
+        ll.addLast(4);
+        
+        ll.add(2, 9);
+        ll.print();
         
     }
 }
